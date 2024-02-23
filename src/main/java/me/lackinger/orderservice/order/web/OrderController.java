dc2ce3527f88cea7946dce03ec8ce8becf8fb256
Ralf Lackinger <ralf.lackinger@anevis-solutions.com>
2024-02-23 12:37:49 +0100

    sync code

--- src/main/java/me/lackinger/orderservice/order/web/OrderController.java
+++ src/main/java/me/lackinger/orderservice/order/web/OrderController.java
@@ -3,6 +3,10 @@ package me.lackinger.orderservice.order.web;
 import jakarta.validation.Valid;
 import me.lackinger.orderservice.order.domain.Order;
 import me.lackinger.orderservice.order.domain.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,6 +15,8 @@ import reactor.core.publisher.Mono;
 @RequestMapping("orders")
 public class OrderController {
 
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
 	private final OrderService orderService;
 
 	public OrderController(OrderService orderService) {
@@ -18,12 +24,14 @@ public class OrderController {
 	}
 
 	@GetMapping
-	public Flux<Order> getAllOrders() {
-		return orderService.getAllOrders();
+	public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+		log.info("Fetching all orders");
+		return orderService.getAllOrders(jwt.getSubject());
 	}
 
 	@PostMapping
 	public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+		log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
 		return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
 	}
 
